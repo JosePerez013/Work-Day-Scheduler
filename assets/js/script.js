de7f98615f61bddffd1a534a8096935dd2c8c760
JosePerez013 <joseperez951@icloud.com>
2023-08-10 12:09:41 -0700

    added js  functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,69 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function () {
+  // variables for elements
+  var dayEl = $("#currentDay");
+  var notifyEl = $("#notify");
+  var presentEl = $(".past");
+
+  // Show current day on top of page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+  // Save button click listener for saving to localStorage
+  $(".saveBtn").on("click", function () {
+    var meetingText = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, meetingText);
+
+    // show notification when saved
+    $(".notification").removeClass("hide");
+    $(".notification").addClass("show");
+
+    // hide notification after 3 seconds
+    setTimeout(function () {
+      $(".notification").removeClass("show");
+      $(".notification").addClass("hide");
+    }, 3000);
+  });
+
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = dayjs().hour();
+    console.log(currentHour);
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // checks if the hour is less than current hour or not or equal to current hour
+      if (blockHour < currentHour) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+        console.log("updated to past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        console.log("updated to present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        console.log("updated to future");
+      }
+    });
+  }
+  //  Calls the hourUpdater function to run every 30 seconds
+  hourUpdater();
+  setInterval(hourUpdater, 30000);
+  // Grab the localStorage for each hour and display to page when user comes back / refreshes
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
